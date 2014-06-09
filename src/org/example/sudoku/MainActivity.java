7c66fd60b86345f3849e12237be9ce78bc98bb0f
neetscs <neetscs@gmail.com>
2014-06-08 20:46:29 -0700

    removing unused imports

--- src/org/example/sudoku/MainActivity.java
+++ src/org/example/sudoku/MainActivity.java
@@ -1,20 +1,16 @@
 package org.example.sudoku;
 
-import android.R.string;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.ViewGroup;
-import android.os.Build;
 
 public class MainActivity extends Activity implements OnClickListener {
 
